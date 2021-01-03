4981708e8cd3130342746b256a23f43dc12efa64
Kartashev Andrey <andrey01_2001@outlook.com>
2021-01-03 22:51:06 +0300

    Project_lesson1_3

---
+++ Level3/project_lesson1_3/src/ru/geekbrains/lesson1/homework/Apple.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.lesson1.homework;
+
+public class Apple extends Fruit {
+
+    public final static float APPLE_WEIGHT = 1.0f;
+
+    public Apple() {
+        super(1.0f);
+    }
+}
